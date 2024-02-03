61e0fac6d9cfaaaf5fe14437adece7b4174afec0
Anuhya Samudrala <anuhyasamudrala@Anuhyas-MacBook-Pro.local>
2024-02-03 13:11:42 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-// Budget API
 
-const express = require('express');
-const cors = require('cors');
+// Budget API
+// adding required dependencies "express" and also enabling cors to ensure web application is accessible from anywhere in the world
+
+const express = require("express");
+// const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
+app.use('/', express.static('public'))
 
 const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
+  myBudget: [
+    {
+      title: "Eat out",
+      budget: 25,
+    },
+    {
+      title: "Rent",
+      budget: 275,
+    },
+    {
+      title: "Grocery",
+      budget: 110,
+    },
+    
+  ],
 };
 
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get("/budget", (req, res) => {
+  res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
